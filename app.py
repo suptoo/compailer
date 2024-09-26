8b19e453f66c42a41dc67bbeffbf837da5acc97f
Umor Faruk Supto <122749150+suptoo@users.noreply.github.com>
2024-09-26 12:16:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ HTML = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Super Interactive Python Compiler</title>
+    <title>Learning Phobia Python Compiler</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ext-language_tools.js"></script>
     <style>
